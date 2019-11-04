78c21bb1b8bbf18303ac2fd2a11b50a9468fe326
Codave <Codaveai@outlook.com>
2019-11-04 22:47:00 +0800

    fitst commit

---
+++ 409.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+	int longestPalindrome(string s) {
+		int char_map[128] = { 0 };
+		int max_length = 0;
+		int flag = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char_map[s[i]]++;
+		}
+		for (int i = 0; i < 128; i++) {
+			if (char_map[i] % 2 == 0) {
+				max_length += char_map[i];
+			}
+			else {
+				max_length += char_map[i]-1;
+				flag = 1;
+			}
+		}
+		return max_length + flag;
+	}
+};
+
+int main() {
+
+	string s = "abccccddaa";
+	Solution solve;
+	printf("%d\n", solve.longestPalindrome(s));
+
+	return 0;
+}
