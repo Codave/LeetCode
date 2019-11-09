fb63295d8384fc0d9c6add44a6f09c4e0281ec1d
Codave <Codaveai@outlook.com>
2019-11-09 22:27:06 +0800

    first commit

---
+++ 198.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		if (nums.size() == 0) {
+			return 0;
+		}
+		if (nums.size() == 1) {
+			return nums[0];
+		}
+		vector<int> dp(nums.size(), 0);
+		dp[0] = nums[0];
+		dp[1] = max(nums[0], nums[1]);
+		for (int i = 2; i < nums.size(); i++) {
+			dp[i] = max(dp[i - 2] + nums[i], dp[i-1]);
+		}
+		return dp[nums.size()-1];
+	}
+};
+
+int main() {
+
+	vector<int> nums = { 5,2,6,3,1,7 };
+	Solution solve;
+	printf("%d\n", solve.rob(nums));
+
+	return 0;
+}
