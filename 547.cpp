31ca5255f1498cfd6142f383b7aa1c9bd1887c9b
Codave <Codaveai@outlook.com>
2019-11-15 18:21:02 +0800

    添加解法二:并查集做法

--- 547.cpp
+++ 547.cpp
@@ -2,29 +2,87 @@
 #include<cstdio>
 using namespace std;
 
+//解法一
+//class Solution {
+//public:
+//	int findCircleNum(vector<vector<int>>& M) {
+//		vector<int> visit(M.size(), 0);
+//		int count = 0;
+//		for (int i = 0; i < M.size(); i++) {
+//			if (visit[i] == 0) {
+//				DFS_graph(i, M, visit);
+//				count++;
+//			}
+//		}
+//		return count;
+//	}
+//
+//	void DFS_graph(int u, vector<vector<int>>& graph, vector<int>& visit) {
+//		visit[u] = 1;
+//		for (int i = 0; i < graph[u].size(); i++) {
+//			if (visit[i] == 0 && graph[u][i] == 1) {
+//				DFS_graph(i, graph, visit);
+//			}
+//		}
+//	}
+//
+//};
+
+//解法二
+class DisjointSet {
+public:
+	DisjointSet(int n) {
+		for (int i = 0; i < n; i++) {
+			father.push_back(i);
+			_size.push_back(1);
+		}
+		_count = n;
+	}
+	int find(int x) {
+		while (x != father[x]) {
+			//father[x] = father[father[x]];
+			x = father[x];
+		}
+		return x;
+	}
+	void union_(int a, int b) {
+		int faA = find(a);
+		int faB = find(b);
+		if (faA == faB) {
+			return;
+		}
+		if (_size[faA] < _size[faB]) {
+			father[faA] = faB;
+			_size[faB] += _size[faA];
+		}
+		else {
+			father[faB] = faA;
+			_size[faA] = _size[faB];
+		}
+		_count--;
+	}
+	int count() {
+		return _count;
+	}
+private:
+	vector<int> father;
+	vector<int> _size;
+	int _count;
+};
+
 class Solution {
 public:
 	int findCircleNum(vector<vector<int>>& M) {
-		vector<int> visit(M.size(), 0);
-		int count = 0;
+		DisjointSet disjoint_set(M.size());
 		for (int i = 0; i < M.size(); i++) {
-			if (visit[i] == 0) {
-				DFS_graph(i, M, visit);
-				count++;
+			for (int j = i + 1; j < M.size(); j++) {
+				if (M[i][j]) {
+					disjoint_set.union_(i, j);
+				}
 			}
 		}
-		return count;
+		return disjoint_set.count();
 	}
-
-	void DFS_graph(int u, vector<vector<int>>& graph, vector<int>& visit) {
-		visit[u] = 1;
-		for (int i = 0; i < graph[u].size(); i++) {
-			if (visit[i] == 0 && graph[u][i] == 1) {
-				DFS_graph(i, graph, visit);
-			}
-		}
-	}
-
 };
 
 int main() {
