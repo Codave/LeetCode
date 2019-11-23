b26c85366435c618edbd7159ae657ebe136fcd95
Codave <Codaveai@outlook.com>
2019-11-23 23:00:27 +0800

    添加解法二

--- 88.cpp
+++ 88.cpp
@@ -37,11 +37,13 @@ public:
 		int k = m + n - 1;
 		m--;
 		n--;
-		while (m >= 0 && n >= 0){
-			if (nums1[m] > nums2[n])
-				nums1[k--] = nums1[m--];
-			else
+		while (m >= 0 && n >= 0) {
+			if (nums1[m] < nums2[n]) {
 				nums1[k--] = nums2[n--];
+			}
+			else {
+				nums1[k--] = nums1[m--];
+			}
 		}
 		while (n >= 0) {
 			nums1[k--] = nums2[n--];
