9aee04b48f79a9020b1bd87017f3d91b24b1399d
Codave <Codaveai@outlook.com>
2019-11-24 22:59:58 +0800

    添加解法二,滑动窗口

--- 3.cpp
+++ 3.cpp
@@ -1,34 +1,58 @@
-#include<cstdio>
+#include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
 
+//解法一
+//class Solution {
+//public:
+//	int lengthOfLongestSubstring(string s) {
+//		int begin=0;
+//		int result=0;
+//		string word = "";
+//		int char_map[128] = { 0 };
+//		for (int i = 0; i < s.length(); i++) {
+//			char_map[s[i]]++;
+//			if (char_map[s[i]] == 1) {
+//				word += s[i];
+//				if (result < word.length()) {  //打擂法更新最大值
+//					result = word.length();
+//				}
+//			}
+//			else {
+//				while (char_map[s[i]]>1) {
+//					char_map[s[begin]]--;
+//					begin++;
+//				}
+//				word = "";
+//				for (int j = begin; j <= i; j++) {
+//					word += s[j];
+//				}
+//			}
+//		}
+//		return result;
+//	}
+//};
+
+//解法二
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-		int begin=0;
-		int result=0;
-		string word = "";
-		int char_map[128] = { 0 };
-		for (int i = 0; i < s.length(); i++) {
-			char_map[s[i]]++;
-			if (char_map[s[i]] == 1) {
-				word += s[i];
-				if (result < word.length()) {  //打擂法更新最大值
-					result = word.length();
-				}
+		int freq[256] = { 0 };
+		int l = 0, r = -1;
+		int res = 0;
+
+		while (l < s.size()) {
+			if (r + 1 < s.size() && freq[s[r + 1]] == 0) {
+				freq[s[++r]]++;
 			}
 			else {
-				while (char_map[s[i]]>1) {
-					char_map[s[begin]]--;
-					begin++;
-				}
-				word = "";
-				for (int j = begin; j <= i; j++) {
-					word += s[j];
-				}
+				freq[s[l++]]--;
 			}
+			res = max(res, r - l + 1);
 		}
-		return result;
+
+		return res;
 	}
 };
 
