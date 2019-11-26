6176aedb1707619f8c0df0f4c820abcf82ea6297
Codave <Codaveai@outlook.com>
2019-11-26 22:46:16 +0800

    first commit

---
+++ 219.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		unordered_set<int> record;
+		for (int i = 0; i < nums.size(); i++) {
+			if (record.find(nums[i]) != record.end()) {
+				return true;
+			}
+			record.insert(nums[i]);
+			if (record.size() == k + 1) {
+				record.erase(nums[i - k]);
+			}
+		}
+		return false;
+	}
+};
+
+int main() {
+	vector<int> nums = { 1,2,3,1 };
+	bool res = Solution().containsNearbyDuplicate(nums, 3);
+	cout << res << endl;
+
+	return 0;
+}
