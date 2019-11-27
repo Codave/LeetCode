3516bc3ce62cc62a1e27061d70ff4aeec5cb127d
Codave <Codaveai@outlook.com>
2019-11-27 14:48:10 +0800

    first commit

---
+++ 83.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (!head || !head->next) return head;
+		ListNode* cur = head;
+		while (cur->next) {
+			ListNode* next = cur->next;
+			if (cur->val == next->val) {
+				next = next->next;
+				cur->next = next;
+			}
+			else {
+				cur = cur->next;
+				next = next->next;
+			}
+			
+		}
+		return head;
+	}
+};
+
+int main() {
+
+	ListNode a(1);
+	ListNode b(2);
+	ListNode c(2);
+	a.next = &b;
+	b.next = &c;
+	ListNode* head = Solution().deleteDuplicates(&a);
+	while (head) {
+		if (head->next) {
+			cout << head->val << "->";
+		}
+		else {
+			cout << head->val << endl;
+		}
+		head = head->next;
+	}
+
+	return 0;
+}
