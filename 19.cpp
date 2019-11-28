03c39f34a37f2be17353c5c807bfc243a5858426
Codave <Codaveai@outlook.com>
2019-11-28 14:14:37 +0800

    first commit

---
+++ 19.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode dummyHead(0);
+		dummyHead.next = head;
+		ListNode* p = &dummyHead;
+		ListNode* q = p;
+		n = n + 1;
+		while (n--) {
+			q = q->next;
+		}
+		while (q) {
+			q = q->next;
+			p = p->next;
+			if (!q) {
+				break;
+			}
+		}
+		p->next = p->next->next;
+		return dummyHead.next;
+	}
+};
+
+int main() {
+	ListNode a(1);
+	ListNode b(2);
+	ListNode c(3);
+	ListNode d(4);
+	ListNode e(5);
+
+	a.next = &b;
+	b.next = &c;
+	c.next = &d;
+	d.next = &e;
+
+	ListNode* head = Solution().removeNthFromEnd(&a, 5);
+	while (head) {
+		if (head->next) {
+			cout << head->val << "->";
+		}
+		else {
+			cout << head->val << "->NULL";
+		}
+		head = head->next;
+	}
+
+	return 0;
+}
