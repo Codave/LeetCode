c8efcba3e446f08e27bd8e14222531e10c8208dd
Codave <Codaveai@outlook.com>
2019-11-28 22:30:45 +0800

    first commit

--- 82.cpp
+++ 82.cpp
@@ -10,18 +10,17 @@ struct ListNode {
 class Solution {
 public:
 	ListNode* deleteDuplicates(ListNode* head) {
-		if (head == NULL || head->next == NULL) return head;
-		ListNode* temp = new ListNode(0);//设置头节点统一操作
-		ListNode* pre, * curr;//前置节点和现结点
-		temp->next = head;//连通头节点和给定链表
-		pre = temp;//指向头节点
-		curr = temp->next;//指向第一个节点
+		if (head == NULL) return head;
+		ListNode dummy(0);//设置头节点
+		dummy.next = head;//连通头节点和给定链表
+		ListNode* pre = &dummy;//指向前置节点
+		ListNode* curr = head;//指向第一个节点
 		while (curr != NULL) {
 			while (curr->next != NULL && (curr->val == curr->next->val)) {
 				curr = curr->next;//如果下一个节点仍然和这个节点相等，则继续向后移动直至curr指向重复的最后一个节点
 			}
-			if (pre->next != curr) {//即存在重复节点
-				pre->next = curr->next;//直接指向重复的最后一个结点的下一个节点。 #此处也可以添加一行代码delete掉那些重复节点的空间
+			if (pre->next != curr) {//如果存在重复节点
+				pre->next = curr->next;//直接指向重复的最后一个结点的下一个节点。
 				curr = curr->next;//指针后移
 			}
 			else {
@@ -29,10 +28,7 @@ public:
 				pre = pre->next;
 			}
 		}
-
-		return temp->next;
-
-
+		return dummy.next;
 	}
 };
 
@@ -41,9 +37,11 @@ int main() {
 	ListNode b(1);
 	ListNode c(1);
 	ListNode d(2);
+	ListNode e(3);
 	a.next = &b;
 	b.next = &c;
 	c.next = &d;
+	d.next = &e;
 
 	ListNode* head = Solution().deleteDuplicates(&a);
 	while (head) {
