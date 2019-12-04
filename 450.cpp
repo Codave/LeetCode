ff79a8455e1dd511ccec9f0d21c8ddfa6c5c1f79
Codave <Codaveai@outlook.com>
2019-12-05 00:17:00 +0800

    添加解法二

--- 450.cpp
+++ 450.cpp
@@ -8,73 +8,131 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
+//解法一
+//class Solution {
+//public:
+//	TreeNode* deleteNode(TreeNode* root, int key) {
+//		TreeNode* cur = root;
+//		TreeNode* pre = root;
+//		while (cur && cur->val != key) {
+//			pre = cur;
+//			if (key < cur->val) {
+//				cur = cur->left;
+//			}
+//			else {
+//				cur = cur->right;
+//			}
+//		}
+//		if (!cur) {
+//			return root;
+//		}
+//		if (!cur->left && !cur->right) {
+//			if (cur == root) {
+//				return NULL;
+//			}
+//			if (pre->left == cur) {
+//				pre->left = NULL;
+//			}
+//			else {
+//				pre->right = NULL;
+//			}
+//		}
+//		else if (cur->left && !cur->right) {
+//			if (cur == root) {
+//				return cur->left;
+//			}
+//			if (pre->left == cur) {
+//				pre->left = cur->left;
+//			}
+//			else {
+//				pre->right = cur->left;
+//			}
+//		}
+//		else if (cur->right && !cur->left) {
+//			if (cur == root) {
+//				return cur->right;
+//			}
+//			if (pre->left == cur) {
+//				pre->left = cur->right;
+//			}
+//			else {
+//				pre->right = cur->right;
+//			}
+//		}
+//		else {
+//			TreeNode* b = cur->left;
+//			while (b->right) {
+//				b = b->right;
+//			}
+//			b->right = cur->right->left;
+//			cur->right->left = cur->left;
+//			if (cur == root) {
+//				return cur->right;
+//			}
+//			if (pre->left == cur) {
+//				pre->left = cur->right;
+//			}
+//			else {
+//				pre->right = cur->right;
+//			}
+//		}
+//		return root;
+//	}
+//};
+
+//解法二
+class Solution
+{
 public:
-	TreeNode* deleteNode(TreeNode* root, int key) {
-		TreeNode* cur = root;
-		TreeNode* pre = root;
-		while (cur && cur->val != key) {
-			pre = cur;
-			if (key < cur->val) {
-				cur = cur->left;
-			}
-			else {
-				cur = cur->right;
-			}
-		}
-		if (!cur) {
+	TreeNode* deleteNode(TreeNode* root, int key)
+	{
+		if (root == nullptr) return nullptr;
+		if (key < root->val)
+		{
+			root->left = deleteNode(root->left, key);
 			return root;
 		}
-		if (!cur->left && !cur->right) {
-			if (cur == root) {
-				return NULL;
-			}
-			if (pre->left == cur) {
-				pre->left = NULL;
-			}
-			else {
-				pre->right = NULL;
-			}
+		else if (key > root->val)
+		{
+			root->right = deleteNode(root->right, key);
+			return root;
 		}
-		else if (cur->left && !cur->right) {
-			if (cur == root) {
-				return cur->left;
+		else
+		{
+			if (root->left == nullptr)
+			{
+				TreeNode* rootRight = root->right;
+				delete root;
+				return rootRight;
 			}
-			if (pre->left == cur) {
-				pre->left = cur->left;
+			if (root->right == nullptr)
+			{
+				TreeNode* rootLeft = root->left;
+				delete root;
+				return rootLeft;
 			}
-			else {
-				pre->right = cur->left;
-			}
+			TreeNode* tmp = minTreeNode(root->right);
+			tmp->right = removeMin(root->right);
+			tmp->left = root->left;
+			delete root;
+			return tmp;
 		}
-		else if (cur->right && !cur->left) {
-			if (cur == root) {
-				return cur->right;
-			}
-			if (pre->left == cur) {
-				pre->left = cur->right;
-			}
-			else {
-				pre->right = cur->right;
-			}
+	}
+private:
+	TreeNode* minTreeNode(TreeNode* root)
+	{
+		if (root->left == nullptr) return root;
+		return minTreeNode(root->left);
+	}
+
+	TreeNode* removeMin(TreeNode* root)
+	{
+		if (root->left == nullptr)
+		{
+			TreeNode* rootRight = root->right;
+			return rootRight;
 		}
-		else {
-			TreeNode* b = cur->left;
-			while (b->right) {
-				b = b->right;
-			}
-			b->right = cur->right->left;
-			cur->right->left = cur->left;
-			if (cur == root) {
-				return cur->right;
-			}
-			if (pre->left == cur) {
-				pre->left = cur->right;
-			}
-			else {
-				pre->right = cur->right;
-			}
-		}
+		root->left = removeMin(root->left);
 		return root;
 	}
 };
