375a2473df24aa5c2ddc5fd438960ad4e54ab6a1
Codave <Codaveai@outlook.com>
2019-12-05 21:07:52 +0800

    first commit

---
+++ 17.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+private:
+	const string letterMap[10] = {
+		" ",	//0
+		"",		//1
+		"abc",	//2
+		"def",	//3
+		"ghi",	//4
+		"jkl",	//5
+		"mno",	//6
+		"pqrs",	//7
+		"tuv",	//8
+		"wxyz"	//9
+	};
+	vector<string> res;
+	void findCombination(const string& digits, int index, const string& s) {
+		if (index == digits.size()) {
+			res.push_back(s);
+			return;
+		}
+		char c = digits[index];
+		string letters = letterMap[c - '0'];
+		for (int i = 0; i < letters.size(); i++) {
+			findCombination(digits, index + 1, s + letters[i]);
+		}
+		return;
+	}
+public:
+	vector<string> letterCombinations(string digits) {
+		res.clear();
+		if (digits.size() == 0) return res;
+		findCombination(digits, 0, "");
+		return res;
+	}
+};
+
+int main() {
+
+	string str = "23";
+	vector<string> res = Solution().letterCombinations(str);
+	for (int i = 0; i < res.size(); i++) {
+		cout << res[i] << endl;
+	}
+
+	return 0;
+}
