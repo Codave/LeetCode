ac455ed12716cd4b16a401597ae4303d94ddf0b9
Codave <Codaveai@outlook.com>
2019-12-06 22:26:16 +0800

    添加debug注释

--- 47.cpp
+++ 47.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
-#include<vector>
-#include<set>
-#include<algorithm>
-using namespace std;
-
-//解法一
+//#include<iostream>
+//#include<vector>
+//#include<set>
+//#include<algorithm>
+//using namespace std;
+//
+////解法一（debug)
 //class Solution {
 //public:
 //	vector<vector<int>> permuteUnique(vector<int>& nums) {
@@ -41,43 +41,79 @@ using namespace std;
 //			out.pop_back();
 //			printf("让num[%d]:%d恢复访问,visit置为0\n", i, nums[i]);
 //			visited[i] = 0;
+//			
+//			while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+//				printf("----------------------------------------------------------\n");
+//				++i;
+//			}
 //		}
 //		return;
 //	}
 //private:
 //	vector<vector<int>> res;
 //};
-
-//解法二(Todo)
-class Solution {
-public:
-	vector<vector<int>> permuteUnique(vector<int>& nums) {
-		permute(nums, 0);
-		return vector<vector<int>>(res.begin(), res.end());
-	}
-	void permute(vector<int>& nums, int start) {
-		if (start >= nums.size()) res.insert(nums);
-		for (int i = start; i < nums.size(); ++i) {
-			if (i != start && nums[i] == nums[start]) continue;
-			swap(nums[i], nums[start]);
-			permute(nums, start + 1);
-			swap(nums[i], nums[start]);
-		}
-	}
-private:
-	set<vector<int>> res;
-};
-
-int main() {
-
-	vector<int> nums = { 1,1,3 };
-	vector<vector<int>> res = Solution().permuteUnique(nums);
-	for (int i = 0; i < res.size(); i++) {
-		for (int j = 0; j < res[i].size(); j++) {
-			cout << res[i][j];
-		}
-		cout << endl;
-	}
-
-	return 0;
-}
+//
+////解法一
+////class Solution {
+////public:
+////	vector<vector<int>> permuteUnique(vector<int>& nums) {
+////
+////		vector<int> out, visited(nums.size(), 0);
+////		sort(nums.begin(), nums.end());
+////		permuteUniqueDFS(nums, 0, visited, out);
+////		return res;
+////	}
+////	void permuteUniqueDFS(vector<int>& nums, int level, vector<int>& visited, vector<int>& out) {
+////		if (level >= nums.size()) {
+////			res.push_back(out);
+////			return;
+////		}
+////		for (int i = 0; i < nums.size(); ++i) {
+////			if (visited[i] == 1) continue;
+////			// if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0) continue;
+////			visited[i] = 1;
+////			out.push_back(nums[i]);
+////			permuteUniqueDFS(nums, level + 1, visited, out);
+////			out.pop_back();
+////			visited[i] = 0;
+////			while (i + 1 < nums.size() && nums[i] == nums[i + 1]) ++i;
+////		}
+////		return;
+////	}
+////private:
+////	vector<vector<int>> res;
+////};
+////
+//////解法二(Todo)
+////class Solution {
+////public:
+////	vector<vector<int>> permuteUnique(vector<int>& nums) {
+////		permute(nums, 0);
+////		return vector<vector<int>>(res.begin(), res.end());
+////	}
+////	void permute(vector<int>& nums, int start) {
+////		if (start >= nums.size()) res.insert(nums);
+////		for (int i = start; i < nums.size(); ++i) {
+////			if (i != start && nums[i] == nums[start]) continue;
+////			swap(nums[i], nums[start]);
+////			permute(nums, start + 1);
+////			swap(nums[i], nums[start]);
+////		}
+////	}
+////private:
+////	set<vector<int>> res;
+////};
+////
+//int main() {
+//
+//	vector<int> nums = { 1,1,3 };
+//	vector<vector<int>> res = Solution().permuteUnique(nums);
+//	for (int i = 0; i < res.size(); i++) {
+//		for (int j = 0; j < res[i].size(); j++) {
+//			cout << res[i][j];
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
