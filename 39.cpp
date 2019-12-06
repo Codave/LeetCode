9e56488272ca7d23b2a9c62cdaf1b6b3b64a3446
Codave <Codaveai@outlook.com>
2019-12-06 20:32:44 +0800

    添加解法二

--- 39.cpp
+++ 39.cpp
@@ -2,30 +2,54 @@
 #include<vector>
 using namespace std;
 
+//解法一
+//class Solution {
+//public:
+//	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//		vector<int> out;
+//		sumDfs(candidates, target, 0, out);
+//		return res;
+//	}
+//	void sumDfs(vector<int>& candidates, int target,int index, vector<int>& out) {
+//		if (target < 0) {
+//			return;
+//		}
+//		if (target == 0) {
+//			res.push_back(out);
+//			return;
+//		}
+//		for (int i = index; i < candidates.size(); i++) {
+//			out.push_back(candidates[i]);
+//			sumDfs(candidates, target - candidates[i], i, out);
+//			out.pop_back();
+//		}
+//		return;
+//	}
+//private:
+//	vector<vector<int>> res;
+//};
+
+
+//解法二（todo)
 class Solution {
+	vector<vector<int>> res;
+	vector<int> t;
 public:
+	void helper(vector<int>& candidates, int target) {
+		for (int i = 0; i < candidates.size(); i++) {
+			if ((candidates[i] > target) || (t.size() && candidates[i] < t[t.size() - 1])) continue;
+			t.push_back(candidates[i]);
+			if (candidates[i] == target) res.push_back(t);
+			else helper(candidates, target - candidates[i]);
+			t.pop_back();
+		}
+	}
 	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-		vector<int> out;
-		sumDfs(candidates, target, 0, out);
+		res.clear();
+		t.clear();
+		helper(candidates, target);
 		return res;
 	}
-	void sumDfs(vector<int>& candidates, int target,int index, vector<int>& out) {
-		if (target < 0) {
-			return;
-		}
-		if (target == 0) {
-			res.push_back(out);
-			return;
-		}
-		for (int i = index; i < candidates.size(); i++) {
-			out.push_back(candidates[i]);
-			sumDfs(candidates, target - candidates[i], i, out);
-			out.pop_back();
-		}
-		return;
-	}
-private:
-	vector<vector<int>> res;
 };
 
 int main() {
