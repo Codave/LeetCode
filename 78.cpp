0c61a302ded019c16007253e192e808ea0956f88
Codave <Codaveai@outlook.com>
2019-12-06 21:49:36 +0800

    添加解法

--- 78.cpp
+++ 78.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 //方法一
-
+//
 //class Solution {
 //public:
 //	vector<vector<int>> subsets(vector<int>& nums) {
@@ -26,8 +27,28 @@ using namespace std;
 //	}
 //};
 
-//方法二
+//解法二
+//class Solution {
+//public:
+//	vector<vector<int> > subsets(vector<int>& S) {
+//		vector<int> out;
+//		sort(S.begin(), S.end());
+//		getSubsets(S, 0, out);
+//		return res;
+//	}
+//	void getSubsets(vector<int>& S, int pos, vector<int>& out) {
+//		res.push_back(out);
+//		for (int i = pos; i < S.size(); ++i) {
+//			out.push_back(S[i]);
+//			getSubsets(S, i + 1, out);
+//			out.pop_back();
+//		}
+//	}
+//private:
+//	vector<vector<int> > res;
+//};
 
+//方法三
 //class Solution {
 //public:
 //	vector<vector<int>> subsets(vector<int>& nums) {
@@ -45,22 +66,39 @@ using namespace std;
 //		return result;
 //	}
 //};
-//
-//int main() {
-//	
-//	vector<int> nums({ 4, 5, 6 });
-//	vector<vector<int>> result;
-//	Solution solve;
-//	result = solve.subsets(nums);
-//	for (int i = 0; i < result.size(); i++) {
-//		if (result[i].size() == 0) {
-//			printf("[]");
+
+//方法四
+//class Solution {
+//public:
+//	vector<vector<int> > subsets(vector<int>& S) {
+//		vector<vector<int> > res(1);
+//		sort(S.begin(), S.end());
+//		for (int i = 0; i < S.size(); ++i) {
+//			int size = res.size();
+//			for (int j = 0; j < size; ++j) {
+//				res.push_back(res[j]);
+//				res.back().push_back(S[i]);
+//			}
 //		}
-//		for (int j = 0; j < result[i].size(); j++) {
-//			printf("[%d]", result[i][j]);
-//		}
-//		printf("\n");
+//		return res;
 //	}
-//
-//	return 0;
-//}
+//};
+
+int main() {
+	
+	vector<int> nums({ 4, 5, 6 });
+	vector<vector<int>> result;
+	Solution solve;
+	result = solve.subsets(nums);
+	for (int i = 0; i < result.size(); i++) {
+		if (result[i].size() == 0) {
+			printf("[]");
+		}
+		for (int j = 0; j < result[i].size(); j++) {
+			printf("[%d]", result[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
