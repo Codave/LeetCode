f7464bda7249704517e1ad77611f11dda93d7c8a
Codave <Codaveai@outlook.com>
2019-12-09 15:48:38 +0800

    first commit

--- 70.cpp
+++ 70.cpp
@@ -2,16 +2,47 @@
 #include<cstdio>
 using namespace std;
 
+//class Solution {
+//public:
+//	int climbStairs(int n) {
+//		vector<int> dp(n + 1, 0);
+//		dp[1] = 1;
+//		dp[2] = 2;
+//		for (int i = 3; i <= n; i++) {
+//			dp[i] = dp[i - 1] + dp[i - 2];
+//		}
+//		return dp[n];
+//	}
+//};
+
+//class Solution {
+//public:
+//	int climbStairs(int n) {
+//		vector<int> dp(n + 1, 0);
+//		dp[1] = 1;
+//		dp[0] = 1;
+//		for (int i = 2; i <= n; i++) {
+//			dp[i] = dp[i - 1] + dp[i - 2];
+//		}
+//		return dp[n];
+//	}
+//};
+
 class Solution {
-public:
-	int climbStairs(int n) {
-		vector<int> dp(n + 3, 0);
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];
+private:
+	vector<int> dp;
+	int calcWays(int n) {
+		if (n == 1)	return 1;
+		if (n == 2) return 2;
+		if (dp[n] == -1) {
+			dp[n] = calcWays(n - 1) + calcWays(n - 2);
 		}
 		return dp[n];
+	}
+public:
+	int climbStairs(int n) {
+		dp = vector<int>(n + 1, -1);
+		return calcWays(n);
 	}
 };
 
