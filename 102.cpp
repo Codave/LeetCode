7331c421c0d32edfe40f9b53a06aa7b29176e61b
Codave <Codaveai@outlook.com>
2019-12-30 14:44:01 +0800

    添加解法二

--- 102.cpp
+++ 102.cpp
@@ -11,29 +11,51 @@ struct TreeNode {
 	TreeNode(int x):val(x),left(NULL),right(NULL){ }
 };
 
+//解法一
+//class Solution {
+//public:
+//	vector<vector<int>> levelOrder(TreeNode* root) {
+//		vector<vector<int>> res;
+//		if (root == NULL) {
+//			return res;
+//		}
+//		queue<pair<TreeNode*, int>> q;
+//		q.push(make_pair(root, 0));
+//		while (!q.empty()) {
+//			TreeNode* node = q.front().first;
+//			int level = q.front().second;
+//			q.pop();
+//			if (level == res.size()) {
+//				res.push_back(vector<int>());
+//			}
+//			res[level].push_back(node->val);
+//			if (node->left) {
+//				q.push(make_pair(node->left, level + 1));
+//			}
+//			if (node->right) {
+//				q.push(make_pair(node->right, level + 1));
+//			}
+//		}
+//		return res;
+//	}
+//};
+
+//解法二
 class Solution {
 public:
 	vector<vector<int>> levelOrder(TreeNode* root) {
+		if (!root) return {};
 		vector<vector<int>> res;
-		if (root == NULL) {
-			return res;
-		}
-		queue<pair<TreeNode*, int>> q;
-		q.push(make_pair(root, 0));
+		queue<TreeNode*> q{ {root} };
 		while (!q.empty()) {
-			TreeNode* node = q.front().first;
-			int level = q.front().second;
-			q.pop();
-			if (level == res.size()) {
-				res.push_back(vector<int>());
+			vector<int> oneLevel;
+			for (int i = q.size(); i > 0; --i) {
+				TreeNode* t = q.front(); q.pop();
+				oneLevel.push_back(t->val);
+				if (t->left) q.push(t->left);
+				if (t->right) q.push(t->right);
 			}
-			res[level].push_back(node->val);
-			if (node->left) {
-				q.push(make_pair(node->left, level + 1));
-			}
-			if (node->right) {
-				q.push(make_pair(node->right, level + 1));
-			}
+			res.push_back(oneLevel);
 		}
 		return res;
 	}
@@ -55,6 +77,7 @@ int main() {
 		for (int j = 0; j < res[i].size(); j++) {
 			cout << res[i][j] << " ";
 		}
+		cout << endl;
 	}
 	return 0;
 }
