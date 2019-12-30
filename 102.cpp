43fb0d2580229639af9fb3ddf43886c8300d8de9
Codave <Codaveai@outlook.com>
2019-12-30 14:55:30 +0800

    添加解法三

--- 102.cpp
+++ 102.cpp
@@ -40,7 +40,24 @@ struct TreeNode {
 //	}
 //};
 
-//解法二
+//解法二（解法一的递归形式）
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> res;
+		levelorder(root, 0, res);
+		return res;
+	}
+	void levelorder(TreeNode* node, int level, vector<vector<int>>& res) {
+		if (!node) return;
+		if (res.size() == level) res.push_back({});
+		res[level].push_back(node->val);
+		if (node->left) levelorder(node->left, level + 1, res);
+		if (node->right) levelorder(node->right, level + 1, res);
+	}
+};
+
+//解法三
 class Solution {
 public:
 	vector<vector<int>> levelOrder(TreeNode* root) {
