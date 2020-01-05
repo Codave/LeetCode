95679b3da417d3c3130adeb2167329c46351c40e
Codave <Codaveai@outlook.com>
2020-01-05 16:29:25 +0800

    添加解法二和解法三

--- 35.cpp
+++ 35.cpp
@@ -2,34 +2,66 @@
 #include<vector>
 using namespace std;
 
+//解法一
+//class Solution {
+//public:
+//	int searchInsert(vector<int>& nums, int target) {
+//		int index = -1;
+//		int begin = 0;
+//		int end = nums.size() - 1;
+//		while (index == -1) {
+//			int mid = (begin + end) / 2;
+//			if (target == nums[mid]) {
+//				index = mid;
+//			}
+//			else if (target < nums[mid]) {
+//				if (mid == 0 || target > nums[mid - 1]) {
+//					index = mid;
+//				}
+//				end = mid - 1;
+//			}
+//			else if (target > nums[mid]) {
+//				if (mid == nums.size() - 1 || target < nums[mid + 1]) {
+//					index = mid + 1;
+//				}
+//				begin = mid + 1;
+//			}
+//		}
+//		return index;
+//	}
+//};
+
+//解法二
 class Solution {
 public:
 	int searchInsert(vector<int>& nums, int target) {
-		int index = -1;
-		int begin = 0;
-		int end = nums.size() - 1;
-		while (index == -1) {
-			int mid = (begin + end) / 2;
-			if (target == nums[mid]) {
-				index = mid;
-			}
-			else if (target < nums[mid]) {
-				if (mid == 0 || target > nums[mid - 1]) {
-					index = mid;
-				}
-				end = mid - 1;
-			}
-			else if (target > nums[mid]) {
-				if (mid == nums.size() - 1 || target < nums[mid + 1]) {
-					index = mid + 1;
-				}
-				begin = mid + 1;
-			}
+		if (nums.empty() || nums.back() < target) return nums.size();
+		int l = 0;
+		int r = nums.size() - 1;
+		while (l < r) {
+			int mid = l + r >> 1;
+			if (nums[mid] < target) l = mid + 1;
+			else r = mid;
 		}
-		return index;
+		return r;
 	}
 };
 
+//解法三
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+		if (nums.empty() || nums.back() < target) return nums.size();
+		int l = 0;
+		int r = nums.size() - 1;
+		while (l < r) {
+			int mid = l + r +1 >> 1;
+			if (nums[mid] >= target) l = mid;
+			else r = mid - 1;
+		}
+		return r;
+	}
+};
 
 int main() {
 	int test[] = { 1,3,5,6 };
