0afa95b732e4f7b2db55cda60e220ef3a4826275
Codave <Codaveai@outlook.com>
2020-01-06 16:49:47 +0800

    first commit

---
+++ 136.cpp
@@ -0,0 +1,28 @@
+//解法一
+class Solution {
+public:
+	int singleNumber(vector<int>& data) {
+		unordered_map<int, int> umap;
+		for (int i = 0; i < data.size(); i++) {
+			umap[data[i]]++;
+		}
+		for (int i = 0; i < data.size(); i++) {
+			if (umap[data[i]] == 1) {
+				return data[i];
+			}
+		}
+		return 0;
+	}
+};
+
+//解法二
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int res = 0;
+		for (auto x : nums) {
+			res ^= x;
+		}
+		return res;
+	}
+};
