9a511664caaf5e14b30296e5090867c8eef904db
Codave <Codaveai@outlook.com>
2020-01-07 19:40:05 +0800

    添加解法二

--- 61.cpp
+++ 61.cpp
@@ -7,6 +7,7 @@ struct ListNode {
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
+//解法一
 class Solution {
 public:
 	ListNode* rotateRight(ListNode* head, int k) {
@@ -24,6 +25,27 @@ public:
 		}
 		p->next = NULL;
 
+		return head;
+	}
+};
+
+//解法二
+class Solution {
+public:
+	ListNode* rotateRight(ListNode* head, int k) {
+		if (!head) return 0;
+		int n = 0;
+		for (auto p = head; p; p = p->next) n++;
+		k %= n;
+		auto first = head, second = head;
+		while (k--) first = first->next;
+		while (first->next) {
+			first = first->next;
+			second = second->next;
+		}
+		first->next = head;
+		head = second->next;
+		second->next = NULL;
 		return head;
 	}
 };
