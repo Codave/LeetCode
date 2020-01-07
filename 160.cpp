ff9ac838f0c9cdf3219c59f0e47b535cd089c0bf
Codave <Codaveai@outlook.com>
2020-01-07 21:54:29 +0800

    添加解法三

--- 160.cpp
+++ 160.cpp
@@ -1,55 +1,53 @@
 #include<iostream>
 using namespace std;
 
-////解法一
-//class Solution {
-//public:
-//	ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
-//		//得到两个链表的长度
-//		ListNode* p1 = pHead1;
-//		ListNode* p2 = pHead2;
-//		int cnt1 = 0, cnt2 = 0;
-//		while (p1) {
-//			cnt1++;
-//			p1 = p1->next;
-//		}
-//		while (p2) {
-//			cnt2++;
-//			p2 = p2->next;
-//		}
-//		p1 = pHead1;
-//		p2 = pHead2;
-//		if (cnt1 > cnt2) {
-//			for (int i = 0; i < cnt1 - cnt2; i++) {
-//				p1 = p1->next;
-//			}
-//		}
-//		else {
-//			for (int i = 0; i < cnt2 - cnt1; i++) {
-//				p2 = p2->next;
-//			}
-//		}
-//		while (p1 && p2) {
-//			if (p1 == p2) {
-//				return p1;
-//			}
-//			p1 = p1->next;
-//			p2 = p2->next;
-//		}
-//		return NULL;
-//	}
-//};
-
-
-//解法二
 struct ListNode {
 	int val;
 	ListNode* next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
+//解法一
+class Solution {
+public:
+	ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
+		//得到两个链表的长度
+		ListNode* p1 = pHead1;
+		ListNode* p2 = pHead2;
+		int cnt1 = 0, cnt2 = 0;
+		while (p1) {
+			cnt1++;
+			p1 = p1->next;
+		}
+		while (p2) {
+			cnt2++;
+			p2 = p2->next;
+		}
+		p1 = pHead1;
+		p2 = pHead2;
+		if (cnt1 > cnt2) {
+			for (int i = 0; i < cnt1 - cnt2; i++) {
+				p1 = p1->next;
+			}
+		}
+		else {
+			for (int i = 0; i < cnt2 - cnt1; i++) {
+				p2 = p2->next;
+			}
+		}
+		while (p1 && p2) {
+			if (p1 == p2) {
+				return p1;
+			}
+			p1 = p1->next;
+			p2 = p2->next;
+		}
+		return NULL;
+	}
+};
 
 
+//解法二
 class Solution {
 public:
 	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
@@ -87,6 +85,31 @@ public:
 			delta--;
 		}
 		return head;
+	}
+};
+
+
+//解法三
+class Solution {
+public:
+	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+		auto p = headA;
+		auto q = headB;
+		while (p != q) {
+			if (p) {
+				p = p->next;
+			}
+			else {
+				p = headB;
+			}
+			if (q) {
+				q = q->next;
+			}
+			else {
+				q = headA;
+			}
+		}
+		return p;
 	}
 };
 
