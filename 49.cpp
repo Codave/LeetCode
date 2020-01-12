c5aa8dd0fd739a1ca78f83091e262166ee83eeb0
Codave <Codaveai@outlook.com>
2020-01-12 21:28:47 +0800

    添加解法三

--- 49.cpp
+++ 49.cpp
@@ -1,6 +1,7 @@
 #include<string>
 #include<vector>
 #include<map>
+#include<unordered_map>
 #include<algorithm>
 using namespace std;
 
@@ -56,6 +57,22 @@ private:
 		for (int i = 0; i < str.length(); i++) {
 			vec[str[i] - 'a']++;
 		}
+	}
+};
+
+//½â·¨Èý
+class Solution {
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs) {
+		unordered_map<string, vector<string>> hash;
+		for (auto str : strs) {
+			string key = str;
+			sort(key.begin(), key.end());
+			hash[key].push_back(str);
+		}
+		vector<vector<string>> res;
+		for (auto item : hash) res.push_back(item.second);
+		return res;
 	}
 };
 
