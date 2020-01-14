18f59450863e55766c42e7091f94f179feb336f6
Codave <Codaveai@outlook.com>
2020-01-14 15:35:31 +0800

    添加解法五

--- 78.cpp
+++ 78.cpp
@@ -84,6 +84,24 @@ using namespace std;
 //	}
 //};
 
+//·½·¨Îå
+class Solution {
+public:
+	vector<vector<int>> subsets(vector<int>& nums) {
+		vector<vector<int>> res;
+		for (int i = 0; i < (1 << nums.size()); i++) {
+			vector<int> now;
+			for (int j = 0; j < nums.size(); j++) {
+				if (i >> j & 1) {
+					now.push_back(nums[j]);
+				}
+			}
+			res.push_back(now);
+		}
+		return res;
+	}
+};
+
 int main() {
 	
 	vector<int> nums({ 4, 5, 6 });
