f23092e86dcb3af0b6960ba7b2cb06b4e00fa8a0
Codave <Codaveai@outlook.com>
2020-02-02 23:04:00 +0800

    增加解法二

--- 26.cpp
+++ 26.cpp
@@ -3,14 +3,32 @@
 #include<algorithm>
 using namespace std;
 
+//解法一
+//class Solution {
+//public:
+//	int removeDuplicates(vector<int>& nums) {
+//		//sort(nums.begin(), nums.end());
+//		for (int i = 1; i < nums.size(); i++) {
+//			if (nums[i - 1] == nums[i]) {
+//				nums.erase(nums.begin() + i);
+//				i--;
+//			}
+//		}
+//		return nums.size();
+//	}
+//};
+
+//解法二
 class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
-		//sort(nums.begin(), nums.end());
-		for (int i = 1; i < nums.size(); i++) {
-			if (nums[i - 1] == nums[i]) {
-				nums.erase(nums.begin() + i);
-				i--;
+		if (nums.empty()) return 0;
+		for (vector<int>::iterator it = nums.begin() + 1; it != nums.end();) {
+			if (*(it - 1) == *it) {
+				it = nums.erase(it);
+			}
+			else {
+				it++;
 			}
 		}
 		return nums.size();
